a4b375e949b4629f4c0e138f3e3bf84350ea61ac
xiayu <674335949@qq.com>
2018-12-30 11:28:50 +0800

    add 图文展示组建

---
+++ src/modules/index/components/graphicDisplay/view.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="gView">
+    <el-row>
+      <el-col :span="10">
+        <div class="block">
+          <span class="demonstration">默认 Hover 指示器触发</span>
+          <el-carousel height="150px">
+            <el-carousel-item v-for="item in 4" :key="item">
+              <h3>{{ item }}</h3>
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+      </el-col>
+      <el-col :span="14">
+        <div>asxs</div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "graphicView",
+    props: {
+      data: {
+        type: Array;
+        dafult: null;
+      }
+    }
+  }
+</script>
+
+<style scoped lang="less">
+
+</style>
